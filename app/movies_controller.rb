544a243e0e13a1f06782bf2f850d95e87284bf39
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-02-26 17:23:39 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -5,6 +5,7 @@
 #   movie = __                     #   movie = Movie.new
 # end                              # end
 
+require 'pry'
 def can_be_instantiated_and_then_saved
   movie = Movie.new
   movie.title = "This is a title."
@@ -19,13 +20,13 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.create(attributes)
+  movie = Movie.find_or_create_by(attributes)
 end
 
 def can_be_created_in_a_block
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1990
+    m.title="Home Alone"
+    m.release_date=1990
   end
 end
 
@@ -38,29 +39,28 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-  Movie.all.size
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find_by(id: 1).title
+  Movie.find(1).title
 end
 
 def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.find_by(title: "Title")
+  Movie.find_or_create_by(title: 'Title', release_date: 2000, director: "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by
+  # For this test return all movies released after 2002 and ordered by 
   # release date descending
   Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
-  Movie.create(title: "Awesome Flick")
   movie = Movie.find_by(title: "Awesome Flick")
   movie.title = "Even Awesomer Flick"
   movie.save
@@ -68,8 +68,7 @@ end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
-  Movie.create(title: "Wat?")
-  movie = Movie.find_by(title: "Wat?")
+  movie = Movie.create(title: "Wat?")
   movie.update(title: "Wat, huh?")
 end
 
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -75,14 +75,6 @@ describe 'Movie' do
         expect(movie.title).to eq("Home Alone")
         expect(movie.release_date).to eq(1990)
       end
-
-      it 'can be created in a block' do
-        args = { title: "The Room", release_date: 2003 }
-        movie = can_be_created_in_a_block(args)
-
-        expect(movie.title).to eq("The Room")
-        expect(movie.release_date).to eq(2003)
-      end
    end
 
     context 'reading' do
@@ -93,32 +85,15 @@ describe 'Movie' do
         Movie.last.update_attribute(:id, 999)
       end
 
-      it 'can get the first item in the database' do
-        movie = can_get_the_first_item_in_the_database
-        expect(movie.title).to eq("Movie_0")
-      end
-
-      it 'can get the last item in the databse' do
-        movie = can_get_the_last_item_in_the_database
-        expect(movie.title).to eq("Movie_4")
-      end
 
       it 'can get size of the database' do
         movies_size = can_get_size_of_the_database
         expect(movies_size).to eq(5)
       end
 
-      it 'can retrive the first item from the database by id' do
-        expect(can_find_the_first_item_from_the_database_using_id.title).to eq("Movie_0")
-      end
-
       it 'can retrieve from the database using different attributes' do
         movie = Movie.create(title: "Title", release_date: 2000, director: "Me")
         expect(can_find_by_multiple_attributes).to eq(movie)
-      end
-
-      it 'can use a where clause and be sorted' do
-        expect(can_find_using_where_clause_and_be_sorted.map{|m| m.title}).to eq(["Movie_4", "Movie_3"])
       end
     end
 
